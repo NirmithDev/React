d88d302818b5517aed6fd268d3cc7220a7427524
Nirmith Victor <63338983+NirmithDev@users.noreply.github.com>
2023-05-21 20:41:33 -0400

    Added Navbar and started on Data page

--- multi page/src/App.js
+++ multi page/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './components/hero/Hero'
+import Navbar from './components/navbar/Navbar';
+import Data from './components/data/Data';
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Hero/>
+      <Data></Data>
     </>
   );
 }
---
+++ multi page/src/components/data/Data.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Data.css'
+
+const Data = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Data
