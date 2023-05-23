a94b20cbb42aa7b353f46c3a7f6cc4890c42c159
Nirmith Victor <63338983+NirmithDev@users.noreply.github.com>
2023-05-23 16:21:20 -0400

    Add more sections to main home page

--- multi page/src/App.js
+++ multi page/src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar';
 import Data from './components/data/Data';
-
+import Cloud from './components/cloud/Cloud';
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero/>
-      <Data></Data>
+      <Data/>
+      <Cloud/>
     </>
   );
 }
--- multi page/src/components/data/Data.js
+++ multi page/src/components/data/Data.js
@@ -3,8 +3,16 @@ import './Data.css'
 
 const Data = () => {
   return (
-    <div>
-      
+    <div className='data'>
+      <div className='container'>
+        <div className='content'>
+          <h2><span>Data</span> Recovery</h2>
+          <p>lorem ipsum is a string of words meant to represent a collection of string. lorem ipsum is a string of words meant to represent a collection of stringslorem ipsum is a string of words meant to represent a collection of stringslorem ipsum is a string of words meant to represent a collection of strings </p>
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
