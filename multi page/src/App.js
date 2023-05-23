0bba50e118729966e34a0ff7a472ca36b606ac35
Nirmith Victor <63338983+NirmithDev@users.noreply.github.com>
2023-05-23 19:58:04 -0400

    added footer to main page

--- multi page/src/App.js
+++ multi page/src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar';
 import Data from './components/data/Data';
 import Cloud from './components/cloud/Cloud';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <Data/>
       <Cloud/>
+      <Footer/>
     </>
   );
 }
--- multi page/src/components/navbar/Navbar.js
+++ multi page/src/components/navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleNav = () => setNav(!nav)
 
     return (
-    <div className='navbar'>
+    <div name='top' className='navbar'>
       <div className='container'>
         <div className='logo'>
             <SiDatabricks className='icon' />
